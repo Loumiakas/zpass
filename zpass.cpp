9fbb234716a1e13178b9e0eb228b6dca485accc4
Ruslan Laskov <loumiakas1@gmail.com>
2017-08-24 17:49:42 +0100

    Addressed compilation issues
    Addressed compilation issues on older gcc based compilers

--- zpass.cpp
+++ zpass.cpp
@@ -1,19 +1,23 @@
+#include <algorithm>
 #include <iostream>
 #include <sstream>
+#include <cstring>
+#include <bitset>
+
 
 #define SEED_LGTH   12
 #define MGC_NUM_1   0x10F0A563L
 #define MGC_NUM_2   7
 
-void ror( unsigned long long &b, unsigned long long c )
+void ror( unsigned long &b, unsigned long c )
 {
 
     std::string b_bin = std::bitset<32>(b).to_string();
     std::string temp = b_bin.substr( b_bin.size() - c ) + 
                        b_bin.substr( 0, b_bin.size() - c );
-    b = std::bitset<32>(temp).to_ullong();
+    b = std::bitset<32>(temp).to_ulong();
 }
-std::string to_upper(unsigned long long x)  
+std::string to_upper(unsigned long x)  
 {
     std::stringstream sstream;
     sstream << std::hex << x;
@@ -25,7 +29,7 @@ std::string to_upper(unsigned long long x)
 int main(int argc, char *argv[])
 {
     if ( argc != 2 ) {
-        std::cout << "ATSE Seed based unlock for Zyxel routers" << std::endl;
+        std::cout << "ATSE Seed based unlock for Eircom's Zyxel routers" << std::endl;
         std::cout << "- Usage: zypass <seed>" << std::endl;
         return 1;
     }
@@ -36,10 +40,10 @@ int main(int argc, char *argv[])
     }
 
     std::string seed = argv[ 1 ];
-    unsigned long long a;
-    unsigned long long b;
-    unsigned long long c;
-    unsigned long long pass;
+    unsigned long a;
+    unsigned long b;
+    unsigned long c;
+    unsigned long pass;
 
     std::istringstream( seed.substr( 0, 6 ) ) >> std::hex >> a;
     b = a + MGC_NUM_1;
@@ -48,8 +52,7 @@ int main(int argc, char *argv[])
     ror( b, c );
     pass = b ^ a;
 
-    std::cout << "Generated password string: " << std::hex
+    std::cout << "PASSWORD: " << std::hex
               << to_upper( pass ) << std::endl;
     return 0;
 }
-
